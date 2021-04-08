2d73205e1727ab92d8fa49056c40475cc276b445
ttrounce <toby.trounce111@gmail.com>
2021-04-08 13:24:32 +0100

    Added small spacing

--- src/main.cpp
+++ src/main.cpp
@@ -25,8 +25,10 @@ void Init()
     InitFreeType();
 
     glEnable(GL_DEPTH_TEST);
+    
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
     
